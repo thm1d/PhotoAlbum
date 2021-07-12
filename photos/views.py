0682c689b849ae9a8012a0287992856b758c270f
Tahmid Rahman <tahmid11q@gmail.com>
2021-07-12 19:27:43 +0600

    dynamic gallery & photo page

--- photos/views.py
+++ photos/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from .models import Category, Photo
 
 def gallery(request):
-    return render(request, 'photos/gallery.html')
+    categories = Category.objects.all()
+    photos = Photo.objects.all()
+    context = {
+        'categories': categories,
+        'photos': photos,
+    }
+    
+    return render(request, 'photos/gallery.html', context)
 
 def addPhoto(request):
     return render(request, 'photos/add-photo.html')
 
 def viewPhoto(request, pk):
-    return render(request, 'photos/photo.html')
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'photos/photo.html', {'photo': photo})
